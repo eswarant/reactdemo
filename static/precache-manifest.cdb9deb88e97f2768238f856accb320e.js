70b5ce17fc57436b046ddbaa646d24d703f93480
eswarant <rteswaran@gmail.com>
2020-07-16 17:51:17 +0530

    Add files via upload

---
+++ static/precache-manifest.cdb9deb88e97f2768238f856accb320e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13faacff1ecc228cbb87f2d543e7510c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d5400a7f11ac88698e20",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e7aff797bf6e787f1f6c",
+    "url": "/static/js/2.62b641a7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.62b641a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5400a7f11ac88698e20",
+    "url": "/static/js/main.988923cc.chunk.js"
+  },
+  {
+    "revision": "660b455d29fe070447e4",
+    "url": "/static/js/runtime-main.37b47311.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
